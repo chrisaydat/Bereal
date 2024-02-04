d8e478af2a6e9e136f33d1120e645daf6c969ead
chrisaydat <chrisaydat@gmail.com>
2024-02-04 15:10:34 +0000

    Initial Commit

---
+++ Bereal/BerealApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerealApp.swift
+//  Bereal
+//
+//  Created by Festus Chris Otopa Ayeh-Datey on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BerealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
