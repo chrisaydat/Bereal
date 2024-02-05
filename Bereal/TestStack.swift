62c6fe6b7b911b25b5b1db164ec2db294327d577
chrisaydat <chrisaydat@gmail.com>
2024-02-05 00:16:53 +0000

    init

---
+++ Bereal/TestStack.swift
@@ -0,0 +1,18 @@
+//
+//  TestStack.swift
+//  Bereal
+//
+//  Created by Festus Chris Otopa Ayeh-Datey on 04/02/2024.
+//
+
+import SwiftUI
+
+struct TestStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestStack()
+}
